31dc2807271ad12b3b4707f73df54f5026a3734a
Ramon Ribeiro <ramon_ramos_ext@carrefour.com>
2023-03-06 23:11:48 -0300

    fix(project): retorna vendor

--- components/minicart/Cart.tsx
+++ components/minicart/Cart.tsx
@@ -7,7 +7,8 @@ import { useUI } from "../../sdk/useUI.ts";
 import CartItem from "./CartItem.tsx";
 import Coupon from "./Coupon.tsx";
 
-const CHECKOUT_URL = "https://constance.vtexcommercestable.com.br/checkout";
+const CHECKOUT_URL =
+  "https://bravtexfashionstore.vtexcommercestable.com.br/checkout";
 
 function Cart() {
   const { displayCart } = useUI();
